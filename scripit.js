fb69cb6285142e1fb2db05c0f834feab779581bd
Gabriela_zilio 21ATImanhã <gabrielacristinazilioherculano@gmail.com>
2024-10-11 11:00:01 +0000

    finalizado

--- scripit.js
+++ scripit.js
@@ -1,4 +1,4 @@
-function Multiplicar () {
+function multiplicar () {
     const n1 = document.querySelector("#n1").value
     const n2 = document.querySelector("#n2").value
     const n3 = document.querySelector("#n3").value
